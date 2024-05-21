ea5be8b9994a7a136e5c44b2a552f369ce5964cc
Matthew Horridge <matthew.horridge@stanford.edu>
2024-05-21 10:53:44 -0700

    Use records for GitHubIssue

--- src/main/java/edu/stanford/protege/github/issues/shared/GitHubIssue.java
+++ src/main/java/edu/stanford/protege/github/issues/shared/GitHubIssue.java
@@ -4,14 +4,10 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.google.auto.value.AutoValue;
-import com.google.auto.value.extension.serializable.SerializableAutoValue;
-import com.google.gwt.user.client.rpc.IsSerializable;
 import edu.stanford.protege.github.GitHubUser;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-import java.io.Serializable;
 import java.time.Instant;
 import java.util.Collections;
 import java.util.List;
@@ -23,95 +19,32 @@ import java.util.Optional;
  * Stanford Center for Biomedical Informatics Research
  * 2023-07-12
  */
-@AutoValue
-@SerializableAutoValue
-@GwtCompatible(serializable = true)
 @JsonIgnoreProperties(ignoreUnknown = true)
-public abstract class GitHubIssue implements IsSerializable, Serializable {
+public record GitHubIssue(@JsonProperty("url") String url,
+                          @JsonProperty("id") long id,
+                          @JsonProperty("node_id") String nodeId,
+                          @JsonProperty("number") int number,
+                          @JsonProperty("title") String title,
+                          @JsonProperty("user") GitHubUser user,
+                          @JsonProperty("labels") List<GitHubLabel> labels,
+                          @JsonProperty("html_url") String htmlUrl,
+                          @JsonProperty("state") GitHubState state,
+                          @JsonProperty("locked") boolean locked,
+                          @JsonProperty("assignee") @Nullable GitHubUser assignee,
+                          @JsonProperty("assignees") List<GitHubUser> assignees,
+                          @JsonProperty("milestone") @Nullable GitHubMilestone milestone,
+                          @JsonProperty("comments") int comments,
+                          @JsonProperty("created_at") Instant createdAt,
+                          @JsonProperty("updated_at") Instant updatedAt,
+                          @JsonProperty("closed_at") Instant closedAt,
+                          @JsonProperty("closed_by") @Nullable GitHubUser closedBy,
+                          @JsonProperty("author_association") GitHubAuthorAssociation authorAssociation,
+                          @JsonProperty("active_lock_reason") String activeLockReason,
+                          @JsonProperty("body") String body,
+                          @JsonProperty("reactions") GitHubReactions reactions,
+                          @JsonProperty("state_reason") @Nullable GitHubStateReason stateReason) {
 
-    public abstract @JsonProperty("url") String url();
-
-    public abstract @JsonProperty("id") long id();
-
-    public abstract @JsonProperty("node_id") String nodeId();
-
-    public abstract @JsonProperty("number") int number();
-
-    public abstract @JsonProperty("title") String title();
-
-    public abstract @JsonProperty("user") GitHubUser user();
-
-    public abstract @JsonProperty("html_url") String htmlUrl();
-
-    public abstract @JsonProperty("labels") List<GitHubLabel> labels();
-
-    public abstract @JsonProperty("state") GitHubState state();
-
-    public abstract @JsonProperty("locked") boolean locked();
-
-    @Nullable
-    public abstract @JsonProperty("assignee") GitHubUser assignee();
-
-    @JsonIgnore
-    @Nonnull
-    public Optional<GitHubUser> getAssignee() {
-        return Optional.ofNullable(assignee());
-    }
-
-    public abstract @JsonProperty("assignees") List<GitHubUser> assignees();
-
-    @Nullable
-    public abstract @JsonProperty("milestone") GitHubMilestone milestone();
-
-    @JsonIgnore
-    @Nonnull
-    public Optional<GitHubMilestone> getMilestone() {
-        return Optional.ofNullable(milestone());
-    }
-
-    public abstract @JsonProperty("comments") int comments();
-
-    public abstract @JsonProperty("created_at") Instant createdAt();
-
-    public abstract @JsonProperty("updated_at") Instant updatedAt();
-
-    public abstract @JsonProperty("closed_at") Instant closedAt();
-
-    @Nullable
-    public abstract @JsonProperty("closed_by") GitHubUser closedBy();
-
-    @JsonIgnore
-    @Nonnull
-    public Optional<GitHubUser> getClosedBy() {
-        return Optional.ofNullable(closedBy());
-    }
-
-    public abstract @JsonProperty("author_association") GitHubAuthorAssociation authorAssociation();
-
-    public abstract @JsonProperty("active_lock_reason") String activeLockReason();
-
-    public abstract @JsonProperty("body") String body();
-
-    @Nullable
-    public abstract @JsonProperty("reactions") GitHubReactions reactions();
-
-    @JsonIgnore
-    @Nonnull
-    public Optional<GitHubReactions> getReactions() {
-        return Optional.ofNullable(reactions());
-    }
-
-    @Nullable
-    public abstract @JsonProperty("state_reason") GitHubStateReason stateReason();
-
-    @JsonIgnore
-    @Nonnull
-    public Optional<GitHubStateReason> getStateReason() {
-        return Optional.ofNullable(stateReason());
-    }
-
-    @JsonCreator
-    public static GitHubIssue get(@JsonProperty("url") String url,
+    public GitHubIssue(@JsonProperty("url") String url,
                        @JsonProperty("id") long id,
                        @JsonProperty("node_id") String nodeId,
                        @JsonProperty("number") int number,
@@ -134,31 +67,108 @@ public abstract class GitHubIssue implements IsSerializable, Serializable {
                        @JsonProperty("body") String body,
                        @JsonProperty("reactions") GitHubReactions reactions,
                        @JsonProperty("state_reason") @Nullable GitHubStateReason stateReason) {
-        return new AutoValue_GitHubIssue(Objects.requireNonNullElse(url, ""),
-                                         id,
-                                         Objects.requireNonNullElse(nodeId, ""),
-                                         number,
-                                         Objects.requireNonNullElse(title, ""),
-                                         Objects.requireNonNullElse(user, GitHubUser.empty()),
-                                         Objects.requireNonNullElse(htmlUrl, ""),
-                                         Objects.requireNonNullElse(labels, Collections.emptyList()),
-                                         Objects.requireNonNullElse(state, GitHubState.OPEN),
-                                         locked,
-                                         assignee,
-                                         Objects.requireNonNullElse(assignees, Collections.emptyList()),
-                                         milestone,
-                                         comments,
-                                         Objects.requireNonNullElse(createdAt, Instant.EPOCH),
-                                         Objects.requireNonNullElse(updatedAt, Instant.EPOCH),
-                                         Objects.requireNonNullElse(closedAt, Instant.EPOCH),
-                                         closedBy,
-                                         Objects.requireNonNullElse(authorAssociation, GitHubAuthorAssociation.NONE),
-                                         Objects.requireNonNullElse(activeLockReason, ""),
-                                         Objects.requireNonNullElse(body, ""),
-                                         reactions,
-                                         stateReason);
+        this.id = id;
+        this.url = Objects.requireNonNullElse(url, "");
+        this.nodeId = Objects.requireNonNullElse(nodeId, "");
+        this.number = number;
+        this.title = Objects.requireNonNullElse(title, "");
+        this.user = Objects.requireNonNullElse(user, GitHubUser.empty());
+        this.htmlUrl = Objects.requireNonNullElse(htmlUrl, "");
+        this.labels = Objects.requireNonNullElse(labels, Collections.emptyList());
+        this.state = Objects.requireNonNullElse(state, GitHubState.OPEN);
+        this.locked = locked;
+        this.assignee = assignee;
+        this.assignees = Objects.requireNonNullElse(assignees, Collections.emptyList());
+        this.milestone = milestone;
+        this.comments = comments;
+        this.createdAt = Objects.requireNonNullElse(createdAt, Instant.EPOCH);
+        this.updatedAt = Objects.requireNonNullElse(updatedAt, Instant.EPOCH);
+        this.closedAt = Objects.requireNonNullElse(closedAt, Instant.EPOCH);
+        this.closedBy = closedBy;
+        this.authorAssociation = Objects.requireNonNullElse(authorAssociation, GitHubAuthorAssociation.NONE);
+        this.activeLockReason = Objects.requireNonNullElse(activeLockReason, "");
+        this.body = Objects.requireNonNullElse(body, "");
+        this.reactions = reactions;
+        this.stateReason = stateReason;
+    }
 
+    @JsonIgnore
+    @Nonnull
+    public Optional<GitHubUser> getAssignee() {
+        return Optional.ofNullable(assignee());
+    }
 
+    @JsonIgnore
+    @Nonnull
+    public Optional<GitHubMilestone> getMilestone() {
+        return Optional.ofNullable(milestone());
+    }
+
+    @JsonIgnore
+    @Nonnull
+    public Optional<GitHubUser> getClosedBy() {
+        return Optional.ofNullable(closedBy());
+    }
+
+    @JsonIgnore
+    @Nonnull
+    public Optional<GitHubReactions> getReactions() {
+        return Optional.ofNullable(reactions());
+    }
+
+    @JsonIgnore
+    @Nonnull
+    public Optional<GitHubStateReason> getStateReason() {
+        return Optional.ofNullable(stateReason());
+    }
+
+    @JsonCreator
+    public static GitHubIssue get(@JsonProperty("url") String url,
+                                  @JsonProperty("id") long id,
+                                  @JsonProperty("node_id") String nodeId,
+                                  @JsonProperty("number") int number,
+                                  @JsonProperty("title") String title,
+                                  @JsonProperty("user") GitHubUser user,
+                                  @JsonProperty("labels") List<GitHubLabel> labels,
+                                  @JsonProperty("htmlUrl") String htmlUrl,
+                                  @JsonProperty("state") GitHubState state,
+                                  @JsonProperty("locked") boolean locked,
+                                  @JsonProperty("assignee") @Nullable GitHubUser assignee,
+                                  @JsonProperty("assignees") List<GitHubUser> assignees,
+                                  @JsonProperty("milestone") @Nullable GitHubMilestone milestone,
+                                  @JsonProperty("comments") int comments,
+                                  @JsonProperty("created_at") Instant createdAt,
+                                  @JsonProperty("updated_at") Instant updatedAt,
+                                  @JsonProperty("closed_at") Instant closedAt,
+                                  @JsonProperty("closed_by") @Nullable GitHubUser closedBy,
+                                  @JsonProperty("author_association") GitHubAuthorAssociation authorAssociation,
+                                  @JsonProperty("active_lock_reason") String activeLockReason,
+                                  @JsonProperty("body") String body,
+                                  @JsonProperty("reactions") GitHubReactions reactions,
+                                  @JsonProperty("state_reason") @Nullable GitHubStateReason stateReason) {
+        return new GitHubIssue(Objects.requireNonNullElse(url, ""),
+                               id,
+                               Objects.requireNonNullElse(nodeId, ""),
+                               number,
+                               Objects.requireNonNullElse(title, ""),
+                               Objects.requireNonNullElse(user, GitHubUser.empty()),
+                               Objects.requireNonNullElse(labels, Collections.emptyList()),
+                               Objects.requireNonNullElse(htmlUrl, ""),
+                               Objects.requireNonNullElse(state, GitHubState.OPEN),
+                               locked,
+                               assignee,
+                               Objects.requireNonNullElse(assignees, Collections.emptyList()),
+                               milestone,
+                               comments,
+                               Objects.requireNonNullElse(createdAt, Instant.EPOCH),
+                               Objects.requireNonNullElse(updatedAt, Instant.EPOCH),
+                               Objects.requireNonNullElse(closedAt, Instant.EPOCH),
+                               closedBy,
+                               Objects.requireNonNullElse(authorAssociation, GitHubAuthorAssociation.NONE),
+                               Objects.requireNonNullElse(activeLockReason, ""),
+                               Objects.requireNonNullElse(body, ""),
+                               reactions,
+                               stateReason);
 
 
     }
